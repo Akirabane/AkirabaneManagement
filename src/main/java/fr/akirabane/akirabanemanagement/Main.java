5fb4f10db2d47f1a9311f637ab9df6191fc8a282
Akirabane <joshuabenon988@yahoo.fr>
2023-03-06 11:46:40 +0100

    added changes on commands ans listeners

--- src/main/java/fr/akirabane/akirabanemanagement/Main.java
+++ src/main/java/fr/akirabane/akirabanemanagement/Main.java
@@ -2,7 +2,9 @@ package fr.akirabane.akirabanemanagement;
 
 import fr.akirabane.akirabanemanagement.commands.players.Ping;
 import fr.akirabane.akirabanemanagement.commands.players.VoteKick;
+import fr.akirabane.akirabanemanagement.commands.staff.Freeze;
 import fr.akirabane.akirabanemanagement.commands.staff.Staff;
+import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerFirstTime;
 import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerJoin;
 import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerQuit;
 import fr.akirabane.akirabanemanagement.listeners.staff.StaffMode;
@@ -24,11 +26,13 @@ public final class Main extends JavaPlugin {
 
         //command staff
         getCommand("staff").setExecutor(new Staff());
+        getCommand("freeze").setExecutor(new Freeze());
 
         //LISTENERS
         //player listeners
         getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new OnPlayerQuit(), this);
+        getServer().getPluginManager().registerEvents(new OnPlayerFirstTime(), this);
 
         //staff listeners
         getServer().getPluginManager().registerEvents(new StaffMode(), this);
