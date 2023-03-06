e71db70fefae2a8f79dfe906bc9ce365d3752d81
Akirabane <joshuabenon988@yahoo.fr>
2023-03-06 11:36:55 +0100

    improved staffMode

--- src/main/java/fr/akirabane/akirabanemanagement/listeners/staff/StaffMode.java
+++ src/main/java/fr/akirabane/akirabanemanagement/listeners/staff/StaffMode.java
@@ -1,5 +1,6 @@
 package fr.akirabane.akirabanemanagement.listeners.staff;
 
+import fr.akirabane.akirabanemanagement.commands.staff.Staff;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -7,6 +8,26 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class StaffMode implements Listener {
     @EventHandler
     public void onStaffMode(PlayerInteractEvent e) {
-        
+
+        if(Staff.getStaffMode() == false) {
+            return;
+        }
+
+        if(e.getAction().name().contains("LEFT")) {
+            e.setCancelled(true);
+            return;
+        }
+
+        //check item name and create siwtch case depending on the item name
+        String itemName = e.getItem().getItemMeta().getDisplayName();
+        switch (itemName) {
+            case "§cMode Staff":
+                break;
+            case "§cQuitter le mode staff":
+                e.getPlayer().performCommand("staff");
+                break;
+            default:
+                break;
+        }
     }
 }
