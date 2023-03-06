414682b249cb4d8d8eaffb3bf6b68f1a5d484e8a
Akirabane <joshuabenon988@yahoo.fr>
2023-03-06 11:46:07 +0100

    added freeze command

---
+++ src/main/java/fr/akirabane/akirabanemanagement/commands/staff/Freeze.java
@@ -0,0 +1,13 @@
+package fr.akirabane.akirabanemanagement.commands.staff;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Freeze implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
+}
