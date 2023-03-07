b11f26141823475a6498690ab6336311ccf3b436
Akirabane <joshuabenon988@yahoo.fr>
2023-03-07 11:07:12 +0100

    added commands ans listeners

--- src/main/java/fr/akirabane/akirabanemanagement/Main.java
+++ src/main/java/fr/akirabane/akirabanemanagement/Main.java
@@ -1,10 +1,12 @@
 package fr.akirabane.akirabanemanagement;
 
 import fr.akirabane.akirabanemanagement.commands.players.Ping;
+import fr.akirabane.akirabanemanagement.commands.players.ShopCommand;
 import fr.akirabane.akirabanemanagement.commands.players.VoteKick;
 import fr.akirabane.akirabanemanagement.commands.staff.Freeze;
 import fr.akirabane.akirabanemanagement.commands.staff.Staff;
 import fr.akirabane.akirabanemanagement.db.DatabaseManager;
+import fr.akirabane.akirabanemanagement.listeners.players.KitShopMenu;
 import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerFirstTime;
 import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerJoin;
 import fr.akirabane.akirabanemanagement.listeners.players.OnPlayerQuit;
@@ -30,6 +32,7 @@ public final class Main extends JavaPlugin {
         //player commands
         getCommand("ping").setExecutor(new Ping());
         getCommand("Votekick").setExecutor(new VoteKick());
+        getCommand("boutique").setExecutor(new ShopCommand());
 
         //command staff
         getCommand("staff").setExecutor(new Staff());
@@ -40,6 +43,7 @@ public final class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new OnPlayerQuit(), this);
         getServer().getPluginManager().registerEvents(new OnPlayerFirstTime(), this);
+        getServer().getPluginManager().registerEvents(new KitShopMenu(), this);
 
         //staff listeners
         getServer().getPluginManager().registerEvents(new StaffMode(), this);
