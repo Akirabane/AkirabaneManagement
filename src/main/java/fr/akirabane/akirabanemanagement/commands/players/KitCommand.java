1af69801b5c9abb7b30dc908b354cfc0d45bb155
Akirabane <joshuabenon988@yahoo.fr>
2023-03-15 15:18:43 +0100

    createed a /kit command

--- src/main/java/fr/akirabane/akirabanemanagement/commands/players/KitCommand.java
+++ src/main/java/fr/akirabane/akirabanemanagement/commands/players/KitCommand.java
@@ -6,15 +6,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class ShopCommand implements CommandExecutor {
+public class KitCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-
+        // TODO Auto-generated method stub
         if(!(sender instanceof Player)) {
             return true;
         }
 
-        Player player = (Player) sender;
+        Player player = (Player)sender;
 
         if(args.length == 0) {
             new KitShopMenu().open(player);
